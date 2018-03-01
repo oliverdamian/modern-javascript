bbab104fc6eab54a2fe0908f56a74f1f9a249df2
JS Robert Ouimet <jouimet@kivuto.com>
2018-03-01 13:30:14 -0500

    Added async-await project.

---
+++ async-await/app.js
@@ -0,0 +1,30 @@
+async function myFunc() {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Hello!'), 1000);
+  });
+
+  const err = true;
+
+  if(!err) {
+    const res = await promise;
+    return res;
+  }
+  else {
+    await Promise.reject(new Error('Something went wrong...'));
+  }
+
+}
+
+myFunc()
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+async function getUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  return data;
+}
+
+getUsers()
+  .then(users => console.log(users));
